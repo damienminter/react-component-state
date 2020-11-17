d80829f58021013a664576632a37cb882c061635
Damien Minter <Damo@MacBook-Pro-2.local>
2020-11-17 15:22:12 +1100

    Add new to child update via grandchild with rest

--- src/Components/ChildA.js
+++ src/Components/ChildA.js
@@ -2,10 +2,20 @@ import React from "react";
 import GrandchildA from "./GrandchildA";
 
 export default function ChildA(props) {
+  const [change, setChange] = React.useState("");
+  // Use spread operator to add change and handleChange to props
+  props = { change, handleChange, ...props };
+  console.log(props); // change, handleChange, state, handleClick
+
+  function handleChange(e) {
+    setChange(e.target.value);
+  }
+
   return (
     <div>
       <div className="container">
         <h1>This is a Child</h1>
+        <p>{change}</p>
       </div>
       <GrandchildA {...props} />
     </div>
--- src/Components/GrandchildA.js
+++ src/Components/GrandchildA.js
@@ -1,11 +1,14 @@
 import React from "react";
 import GreatGrandchildA from "./GreatGrandchildA";
 
-export default function GrandchildA(props) {
+// Use rest operator to extract change and handleChange into thsi component and leave state and handleClick for the next component
+export default function GrandchildA({ change, handleChange, ...props }) {
+  console.log(props); // state, handleClick
   return (
     <div>
       <div className="container">
         <h1>This is a Grandchild</h1>
+        <input type="text" value={change} onChange={handleChange}></input>
       </div>
       <GreatGrandchildA {...props} />
     </div>
