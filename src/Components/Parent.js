a3afa5b89775b4672c1970a619ac5fd21e89102a
Damien Minter <Damo@MacBook-Pro-2.local>
2020-11-18 03:22:47 +1100

    Add Callback to setStateFunction

--- src/Components/Parent.js
+++ src/Components/Parent.js
@@ -2,10 +2,10 @@ import React, { useState } from "react";
 import ChildA from "./ChildA";
 
 export default function Parent() {
-  const [state, setState] = useState("");
+  const [state, setState] = useState("None");
 
-  function handleClick() {
-    setState("State Updated");
+  function handleClick(comp) {
+    setState(comp);
     console.log("clicked");
   }
 
@@ -13,7 +13,10 @@ export default function Parent() {
     <div>
       <div className="container">
         <h1>This is a parent</h1>
-        <h1>Parent state {state}</h1>
+        <h1>Parent state = {state}</h1>
+        <button type="click" onClick={() => handleClick("Parent")}>
+          Update State
+        </button>
       </div>
       <ChildA state={state} handleClick={handleClick} />
     </div>
