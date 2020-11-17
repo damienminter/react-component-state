a41688e79fcb37dd8dd908f9932a0e0e254a76de
Damien Minter <Damo@MacBook-Pro-2.local>
2020-11-18 02:42:11 +1100

    handle event using context

--- src/Components/GreatGrandchildA.js
+++ src/Components/GreatGrandchildA.js
@@ -1,14 +1,17 @@
 import React, { useContext } from "react";
+
+// Import Context
 import { Context } from "../Components/Parent";
 
 export default function GreatGrandchildA() {
-  const { state, setState } = useContext(Context);
+  // Destructure Context
+  const { state, handleClick } = useContext(Context);
   return (
     <div>
       <div className="container">
         <h1>This is great GreatGrandchild A</h1>
-        <h1>GreatGrandchild state {state}</h1>
-        <button type="click" onClick={() => setState("New State")}>
+        <h1>GreatGrandchild state = {state}</h1>
+        <button type="click" onClick={() => handleClick("GreatGrandchild")}>
           Update State
         </button>
       </div>
--- src/Components/Parent.js
+++ src/Components/Parent.js
@@ -5,20 +5,22 @@ import ChildA from "./ChildA";
 export const Context = React.createContext();
 
 export default function Parent() {
-  const [state, setState] = useState("GoodBye");
-  const value = { state, setState };
+  const [state, setState] = useState("None");
+  // Set context initial values to be state and function
+  const value = { state, handleClick };
 
-  function handleClick() {
-    setState("State Updated");
+  function handleClick(component) {
+    setState(component);
     console.log("clicked");
   }
 
   return (
+    // Wrap compoents in Context using provider and initialise
     <Context.Provider value={value}>
       <div className="container">
         <h1>This is a parent</h1>
-        <h1>Parent state {state}</h1>
-        <button type="click" onClick={handleClick}>
+        <h1>Parent state = {state}</h1>
+        <button type="click" onClick={() => handleClick("Parent")}>
           Update State
         </button>
       </div>
