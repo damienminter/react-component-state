868d65414f746c8a5b0aa74a12db67d45f49250b
Damien Minter <Damo@MacBook-Pro-2.local>
2020-11-18 03:19:59 +1100

    Update with callback

--- src/Components/GrandchildA.js
+++ src/Components/GrandchildA.js
@@ -8,8 +8,8 @@ export default function GrandchildA(props) {
     <div>
       <div className="container">
         <h1>This is a Grandchild</h1>
-        <h1>Grandchild state {state}</h1>
-        <button type="click" onClick={handleClick}>
+        <h1>Grandchild state = {state}</h1>
+        <button type="click" onClick={() => handleClick("Grandchild")}>
           Update State
         </button>
       </div>
